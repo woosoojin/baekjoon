a6c749baf5f9954f7f9735df716be1c9631d645e
woosoojin <wsj1998@naver.com>
2022-04-04 23:00:47 +0900

    resolve error

--- cpp/11727.cpp
+++ cpp/11727.cpp
@@ -7,10 +7,10 @@ int dp(int n) {
 	if (n == 1)
 		return 1;
 	if (n == 2)
-		return 2;
-	if (!d[n])
+		return 3;
+	if (d[n] != 0)
 		return d[n];
-	
+
 	return d[n] = (dp(n - 1) + 2 * dp(n - 2)) % 10007;
 }
 
